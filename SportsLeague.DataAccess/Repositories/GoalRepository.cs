4da47cdb373c3d41baae82fa9ea80ab0d0c80d94
AndresVM1 <andresvilla6701@gmail.com>
2026-04-23 19:00:12 -0500

    Phase 5 (First Part) - New 3 entities created

---
+++ SportsLeague.DataAccess/Repositories/GoalRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using SportsLeague.DataAccess.Context;
+
+using SportsLeague.Domain.Entities;
+
+using SportsLeague.Domain.Interfaces.Repositories;
+
+
+
+namespace SportsLeague.DataAccess.Repositories;
+
+
+
+public class GoalRepository : GenericRepository<Goal>, IGoalRepository
+
+{
+
+    public GoalRepository(LeagueDbContext context) : base(context) { }
+
+
+
+    public async Task<IEnumerable<Goal>> GetByMatchAsync(int matchId)
+
+    {
+
+        return await _dbSet
+
+            .Where(g => g.MatchId == matchId)
+
+            .OrderBy(g => g.Minute)
+            // .OrderByDescending(g => g.Minute) // If you want the latest goals first
+            .ToListAsync();
+
+    }
+
+
+
+    public async Task<IEnumerable<Goal>> GetByMatchWithDetailsAsync(int matchId)
+
+    {
+
+        return await _dbSet
+
+            .Where(g => g.MatchId == matchId)
+
+            .Include(g => g.Player)
+
+            // .ThenInclude(p => p.Team)
+
+            // .ThenInclude(t => t.TournamentTeams)
+
+            //.ThenInclude(tt => tt.Tournament)
+
+            .OrderBy(g => g.Minute)
+
+            .ToListAsync();
+
+    }
+
+}
